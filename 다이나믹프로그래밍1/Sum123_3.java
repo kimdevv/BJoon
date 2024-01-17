63b718cf42de9e2f7c616f60537d826f8853241e
kimdevv <129609928+kimdevv@users.noreply.github.com>
2024-01-17 15:48:31 +0900

    다이나믹프로그래밍1

--- 다이나믹프로그래밍1/Sum123_3.java
+++ 다이나믹프로그래밍1/Sum123_3.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Sum123_3 {
   
     static int MOD = 1000000009;
 
