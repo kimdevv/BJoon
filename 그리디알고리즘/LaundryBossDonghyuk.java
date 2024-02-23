b5390f53f314cfe2c208d865a6040c972849ac95
kimdevv <129609928+kimdevv@users.noreply.github.com>
2024-02-23 15:34:43 +0900

    그리디알고리즘

---
+++ 그리디알고리즘/LaundryBossDonghyuk.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.lang.StringBuilder;
+
+public class LaundryBossDonghyuk {
+
+    public static int stoi(String str) {
+        return Integer.parseInt(str);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = stoi(br.readLine());
+        
+        for (int i=0; i<T; i++) {
+          int C = stoi(br.readLine());
+          
+          int quarter = C / 25;
+          C %= 25;
+          
+          int dime = C / 10;
+          C %= 10;
+          
+          int nickel = C / 5;
+          C %= 5;
+          
+          int penny = C / 1;
+          
+          System.out.println(quarter + " " + dime + " " + nickel + " " + penny);
+        }
+    }
+}
