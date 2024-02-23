022c47cf7e8488f3c8ce1f1dd3d88f51ddaa4ebe
kimdevv <129609928+kimdevv@users.noreply.github.com>
2024-02-23 11:11:32 +0900

    그리디알고리즘

---
+++ 그리디알고리즘/Rope.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.lang.StringBuilder;
+
+public class Rope {
+
+    public static int stoi(String str) {
+        return Integer.parseInt(str);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = stoi(br.readLine());
+        
+        int[] weight = new int[N];
+        for (int i=0; i<N; i++) {
+          weight[i] = stoi(br.readLine());
+        }
+        
+        Arrays.sort(weight);
+        
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<N; i++) {
+          int tmp = weight[i] * (N-i);
+          max = Math.max(max, tmp);
+        }
+        
+        System.out.println(max);
+    }
+}
