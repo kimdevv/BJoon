0298264097e56022a0af2c84762fe424e534ab4b
kimdev <kim__dev@hufs.ac.kr>
2024-09-14 22:42:56 +0900

    백준 20240914

---
+++ 정렬/SortingNumber.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class SortingNumber {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = scanner.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i=0; i<count; i++) {
+            pq.add(scanner.nextInt());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()) {
+            sb.append(pq.poll() + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
