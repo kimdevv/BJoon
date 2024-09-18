f2318114aec0ad48be33a51b5901d0f6ddd6fa84
kimdev <kim__dev@hufs.ac.kr>
2024-09-18 19:43:31 +0900

    백준 20240918

--- 정렬/TwoLiquids.java
+++ 정렬/TwoLiquids.java
@@ -9,33 +9,37 @@ public class TwoLiquids {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = stoi(br.readLine()); // 전체 용액의 개수
+        int N = stoi(br.readLine()); // 전체 용액의 수
         int[] array = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0; i<N; i++) {
             array[i] = stoi(st.nextToken());
         }
-        Arrays.sort(array);
+        Arrays.sort(array); // 오름차 순으로 정렬 (-99, -2, -1, 4, 98)
 
-        int closer = Integer.MAX_VALUE;
-        int one = 0;
-        int two = 0;
+        // 양끝에서 가운데로 이동하는 투포인터
+        int leftPointer = 0;
+        int rightPointer = N-1;
 
-        first: for (int i=0; i<=N/2; i++) {
-            for (int j=N-1; j>i; j--) {
-                //System.out.println(i + " " + j);
-                int tempSum = Math.abs(array[j] + array[i]);
-                if (closer >= tempSum) {
-                    closer = tempSum;
-                    one = array[i];
-                    two = array[j];
-                } else {
-                    continue first;
-                }
+        int resultSum = Integer.MAX_VALUE;
+        int resultLeft = 0;
+        int resultRight = 0;
+        while(leftPointer < rightPointer) {
+            int tempSum = array[leftPointer] + array[rightPointer];
+            if (resultSum > Math.abs(tempSum)) {
+                resultSum = Math.abs(tempSum);
+                resultLeft = array[leftPointer];
+                resultRight = array[rightPointer];
+            }
+
+            if (tempSum > 0) { // 합계가 양수면 값이 더 작아져야 함
+                rightPointer--;
+            } else { // 합계가 음수면 값이 더 커져야 함
+                leftPointer++;
             }
         }
 
-        System.out.println(one + " " + two);
+        System.out.println(resultLeft + " " + resultRight);
     }
 }
