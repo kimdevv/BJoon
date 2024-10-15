b13a80d70038a66dda572c0337feefe270de61d9
kimdev <kim__dev@hufs.ac.kr>
2024-10-15 12:54:02 +0900

    백준 20241015

--- 이분탐색/DragonAndDungeon_16434.java
+++ 이분탐색/DragonAndDungeon_16434.java
@@ -1,2 +1,9 @@
+import java.io.*;
+import java.util.*;
+
 public class DragonAndDungeon_16434 {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    }
 }
