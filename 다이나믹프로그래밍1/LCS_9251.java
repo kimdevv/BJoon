b8871832a7decf9a55307b846598f33fc56b8234
Hyunwook Kim <129609928+kimdevv@users.noreply.github.com>
2025-01-06 14:13:39 +0900

    백준 20250106

--- 다이나믹프로그래밍1/LCS_9251.java
+++ 다이나믹프로그래밍1/LCS_9251.java
@@ -2,27 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class LCS_9251 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] stringArray = new String[2];
-        stringArray[0] = bufferedReader.readLine();
-        stringArray[1] = bufferedReader.readLine();
-
-        int longer = stringArray[0].length() >= stringArray[1].length() ? 0 : 1;
-        int shorter = longer == 0 ? 1 : 0;
-        int[] dpArray = new int[stringArray[longer].length() + 1];
-
-        int lastIndex = 0;
-        for (int i=0; i<stringArray[shorter].length(); i++) {
-            char findChar = stringArray[shorter].charAt(i);
-            for (int j=1; j<stringArray[longer].length()+1; j++) {
-                int findIndex = stringArray[longer].indexOf(findChar, lastIndex);
-
-            }
-            System.out.println(Arrays.toString(dpArray));
+  private static String string1;
+  private static String string2;
+  private static int[][] dp;
+  
+  public static void main(String[] args) throws IOException {
+    inputStrings();
+    processDP();
+    outputResult();
+  }
+  
+  private static void inputStrings() throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    string1 = bufferedReader.readLine();
+    string2 = bufferedReader.readLine();
+  }
+  
+  private static void processDP() {
+    dp = new int[string1.length()+1][string2.length()+1];
+    for (int i=1; i<dp.length; i++) {
+      char alphabet = string1.charAt(i-1);
+      for (int j=1; j<dp[i].length; j++) {
+        if (alphabet == string2.charAt(j-1)) {
+          dp[i][j] = dp[i-1][j] + 1;
+        } else {
+          dp[i][j] = dp[i][j-1];
         }
-
-        System.out.println(dpArray[dpArray.length-1]);
+      }
     }
+  }
+  
+  private static void outputResult() {
+    System.out.println(Arrays.toString(dp[0]));
+    System.out.println(Arrays.toString(dp[1]));
+    System.out.println(Arrays.toString(dp[2]));
+    System.out.println(Arrays.toString(dp[3]));
+    System.out.println(Arrays.toString(dp[4]));
+    System.out.println(Arrays.toString(dp[5]));
+    System.out.println(Arrays.toString(dp[6]));
+  }
 }
