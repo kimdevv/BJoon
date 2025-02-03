c0f59386c52dac89b6f0de127b45793de8068f7f
kimdev <kim__dev@hufs.ac.kr>
2025-02-03 22:52:12 +0900

    백준 20250203

--- 그래프탐색/ShortestRoute_1753.java
+++ 그래프탐색/ShortestRoute_1753.java
@@ -1,91 +1,91 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-import java.util.Queue;
 
 public class ShortestRoute_1753 {
-    static int V; // 정점의 개수
-    static int E; // 간선의 개수
+    private static BufferedReader bufferedReader;
+    private static int V; // 정점의 개수
+    private static int E; // 간선의 개수
+    private static int K; // 시작 정점의 번호
+    private static List<Node>[] graph;
+    private static int[] distance;
 
-    public static class Node {
-        int end;
-        int weight;
+    public static void main(String[] args) throws IOException {
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        inputVEK();
+        inputNodes();
+        traverseGraph();
+        outputResult();
+        bufferedReader.close();
+    }
 
-        Node(int end, int weight) {
+    private static void inputVEK() throws IOException {
+        StringTokenizer VEK = new StringTokenizer(bufferedReader.readLine());
+        V = Integer.valueOf(VEK.nextToken());
+        E = Integer.valueOf(VEK.nextToken());
+        K = Integer.valueOf(bufferedReader.readLine());
+    }
+
+    private static void inputNodes() throws IOException {
+        graph = new ArrayList[V+1];
+        for (int i=0; i<=V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i=0; i<E; i++) {
+            StringTokenizer rawNode = new StringTokenizer(bufferedReader.readLine()," ");
+            int start = Integer.valueOf(rawNode.nextToken());
+            int end = Integer.valueOf(rawNode.nextToken());
+            int weight = Integer.valueOf(rawNode.nextToken());
+            graph[start].add(new Node(end, weight));
+        }
+    }
+
+    private static void traverseGraph() {
+        distance = new int[V+1];
+        boolean[] isVisited = new boolean[V+1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        Queue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(K, 0));
+        distance[K] = 0;
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            if (isVisited[currentNode.end]) {
+                continue;
+            }
+
+            isVisited[currentNode.end] = true;
+            for (Node nextNode : graph[currentNode.end]) {
+                if (distance[nextNode.end] > distance[currentNode.end] + nextNode.weight) {
+                    distance[nextNode.end] = distance[currentNode.end] + nextNode.weight;
+                    queue.add(new Node(nextNode.end, distance[nextNode.end]));
+                }
+            }
+        }
+    }
+
+    private static void outputResult() {
+        for (int i=1; i<=V; i++) {
+            if (distance[i] == Integer.MAX_VALUE) {
+                System.out.println("INF");
+            } else {
+                System.out.println(distance[i]);
+            }
+        }
+    }
+
+
+    public static class Node implements Comparable<Node> {
+        public int end;
+        public int weight;
+
+        public Node(int end, int weight) {
             this.end = end;
             this.weight = weight;
         }
-    }
 
-    public static int stoi(String str) {
-        return Integer.parseInt(str);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = stoi(st.nextToken());
-        E = stoi(st.nextToken());
-        int startVertex = stoi(br.readLine());
-        int[] weightArray = new int[V+1];
-        boolean[] isVisited = new boolean[V+1];
-        List<Node>[] nodeList = new List[V+1]; // 간선을 담을 리스트. 2차원 배열로 담으면 메모리 초과...
-
-        for (int i=0; i<=V; i++) {
-            if (i == startVertex) {
-                weightArray[i] = 0;
-            } else {
-                weightArray[i] = 1000000;
-            }
-
-            nodeList[i] = new ArrayList<>();
-        }
-
-        for (int i=1; i<=E; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            int tempStart = stoi(st2.nextToken());
-            int tempEnd = stoi(st2.nextToken());
-            int tempWeight = stoi(st2.nextToken());
-            nodeList[tempStart].add(new Node(tempEnd, tempWeight));
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(startVertex);
-
-        while (!queue.isEmpty()) {
-            int nowVertex = queue.poll();
-            isVisited[nowVertex] = true;
-
-            for (int i=0; i<nodeList[nowVertex].size(); i++) {
-                Node nowNode = nodeList[nowVertex].get(i);
-                if (weightArray[nowVertex] + nowNode.weight <= weightArray[nowNode.end]) {
-                    weightArray[nowNode.end] = weightArray[nowVertex] + nowNode.weight;
-                }
-            }
-
-            int nextWeight = 1000000;
-            int nextIdx = 0;
-            for (int i=1; i<=V; i++) {
-                if (weightArray[i] < 1000000 && weightArray[i] <= nextWeight && !isVisited[i]) {
-                    nextWeight = weightArray[i];
-                    nextIdx = i;
-                }
-            }
-            if (nextIdx > 0) {
-                queue.add(nextIdx);
-            }
-        }
-
-        for (int i=1; i<=V; i++) {
-            if (i == startVertex) {
-                System.out.println(0);
-            } else if (weightArray[i] == 1000000) {
-                System.out.println("INF");
-            } else {
-                System.out.println(weightArray[i]);
-            }
+        @Override
+        public int compareTo(Node other) {
+            return this.weight - other.weight;
         }
     }
 }
