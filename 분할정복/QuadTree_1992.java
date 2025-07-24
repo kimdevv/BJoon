1f651b70c7657ec7191121a9e4baad299389a48e
kimdev <kim__dev@hufs.ac.kr>
2025-07-24 12:38:07 +0900

    백준 20250724

---
+++ 분할정복/QuadTree_1992.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class QuadTree_1992 {
+
+    private static int[][] array;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        array = new int[N][N];
+        for (int i=0; i<N; i++) {
+            String line = br.readLine();
+            for (int j=0; j<N; j++) {
+                array[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(divideAndConquer(0, 0, N));
+    }
+
+    private static String divideAndConquer(int startX, int startY, int length) {
+        int color = array[startY][startX];
+        for (int i=0; i<length; i++) {
+            for (int j=0; j<length; j++) {
+                if (array[startY + i][startX + j] != color) {
+                    int halfLength = length / 2;
+                    String sb = "(" + divideAndConquer(startX, startY, halfLength)
+                        + divideAndConquer(startX + halfLength, startY, halfLength)
+                        + divideAndConquer(startX, startY + halfLength, halfLength)
+                        + divideAndConquer(startX + halfLength, startY + halfLength, halfLength)
+                        + ")";
+                    return sb;
+                }
+            }
+        }
+        return color + "";
+    }
+}
