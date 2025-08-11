aa8980228881ffbcb35c08e6caa77d9a01cbb087
kimdev <kim__dev@hufs.ac.kr>
2025-08-11 19:34:26 +0900

    백준 20250811

---
+++ 이분탐색/CuttingLAN_1654.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class CuttingLAN_1654 {
+
+    private static int K; // 현재 갖고 있는 랜선 개수
+    private static int N; // 만들려는 랜선 개수
+    private static int[] cables;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        cables = new int[K];
+        long sum = 0;
+        for (int i=0; i<K; i++) {
+            cables[i] = Integer.parseInt(br.readLine());
+            sum += cables[i];
+        }
+
+        System.out.println(findTreshold(sum));
+    }
+
+    private static long findTreshold(long limit) {
+        long left = 0;
+        long right = (long)Integer.MAX_VALUE * K;
+
+        long maxLength = Long.MIN_VALUE;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (mid == 0) {
+                return 1;
+            }
+
+            long cableCount = calculateCount(mid);
+            if (cableCount < N || limit < mid) {
+                right = mid - 1;
+            } else {
+                maxLength = mid;
+                left = mid + 1;
+            }
+        }
+        return maxLength;
+    }
+
+    private static long calculateCount(long length) {
+        long result = 0;
+        for (long cable : cables) {
+            result += (cable / length);
+        }
+        return result;
+    }
+}
