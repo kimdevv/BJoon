b1319b883f1a0766dd953fcf70419181e9e36a91
kimdev <kim__dev@hufs.ac.kr>
2025-10-30 11:38:40 +0900

    백준 20251030

--- 그래프탐색/Hiding_1697.java
+++ 그래프탐색/Hiding_1697.java
@@ -1,42 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Hiding_1697 {
-    static int N;
-    static int K;
-    static int[] array = new int[200002];
 
-    public static int stoi(String str) {
-        return Integer.parseInt(str);
-    }
+    private static int N; // 수빈이의 현재 위치
+    private static int K; // 동생이 있는 위치
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = stoi(st.nextToken());
-        K = stoi(st.nextToken());
-        for (int i=N; i<=K; i++) {
-            array[i] = i;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int result = -1;
+        boolean[] isVisited = new boolean[200_001];
+        PriorityQueue<Route> pq = new PriorityQueue<>((r1, r2) -> r1.cost - r2.cost);
+        pq.offer(new Route(N, 0));
+        while (!pq.isEmpty()) {
+            Route current = pq.poll();
+            isVisited[current.destination] = true;
+            if (current.destination == K) {
+                result = current.cost;
+                break;
+            }
+
+            if (current.destination + 1 <= 100_000 && !isVisited[current.destination + 1]) {
+                pq.offer(new Route(current.destination + 1, current.cost + 1));
+            }
+            if (current.destination * 2 <= 100_000 && !isVisited[current.destination * 2]) {
+                pq.offer(new Route(current.destination * 2, current.cost + 1));
+            }
+            if (current.destination - 1 >= 0 && !isVisited[current.destination - 1]) {
+                pq.offer(new Route(current.destination - 1, current.cost + 1));
+            }
         }
 
-        dfs(N);
-        System.out.println(array[K]);
+        System.out.println(result);
     }
 
-    public static void dfs(int start) {
-        int[] move = {1, -1, start};
+    private static class Route {
 
-        for (int i=0; i<3; i++) {
-            int cx = array[start] + move[i];
-            if (array[cx] > cx) {
-                array[cx] = cx;
-            }
-            if (cx <= K) {
-                dfs(cx);
-            }
+        private int destination;
+        private int cost;
+
+        private Route(int destination, int cost) {
+            this.destination = destination;
+            this.cost = cost;
         }
     }
 }
